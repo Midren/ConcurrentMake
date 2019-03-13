2bf30183cca0c5860214a5cfe41fa3ec80f9c1ce
RomanMilishchuk <milromchuk@gmail.com>
2019-03-13 23:58:04 +0200

    Running command using ssh

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int scp_write(ssh::Session &session) {
         std::cout << "Eror initializing scp session\n";
         ssh_scp_free(scp);
     } else {
+
         ssh_scp_close(scp);
         ssh_scp_free(scp);
     }
@@ -22,6 +23,28 @@ int scp_write(ssh::Session &session) {
 int main() {
     ssh::Session session;
     session.setOption(SSH_OPTIONS_HOST, "midren@localhost");
-    scp_write(session);
+    try {
+        session.connect();
+    } catch (ssh::SshException &e) {
+        std::cout << e.getError();
+    }
+    char *password = getpass("Password: ");
+    if (ssh_userauth_password(session.getCSession(), NULL, password) != SSH_AUTH_SUCCESS) {
+        std::cout << "Wrong password";
+    }
+
+    ssh::Channel channel(session);
+    channel.openSession();
+    ssh_channel_request_pty(channel.getCChannel());
+    channel.requestExec("ls");
+    int nbytes;
+    char buffer[256];
+    nbytes = ssh_channel_read(channel.getCChannel(), buffer, sizeof(buffer), 0);
+    while (nbytes > 0) {
+        if (write(1, buffer, nbytes) != (unsigned int) nbytes) {
+            return SSH_ERROR;
+        }
+        nbytes = ssh_channel_read(channel.getCChannel(), buffer, sizeof(buffer), 0);
+    }
     return 0;
 }
